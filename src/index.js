10d0685c392cdeebbf064454fcd35a8e534b15f5
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-09-06 19:34:35 -0400

    Print to-do list to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import { createProject, getProjectNames, getProjectTodos } from "./project";
+import { createTodo } from "./todo";
 
+const displayController = (function() {
+  const printTodoList = () => {
+    console.log("TO-DO LIST")
+    for (const projectName of getProjectNames()) {
+      console.log("Project:", projectName);
+
+      const todos = getProjectTodos(projectName);
+      if (todos.length === 0) {
+        console.log("No tasks yet")
+      } else {
+        for (const todo of todos) {
+          console.log(todo);
+        }
+      }
+    }
+  }
+
+  printTodoList();
+})();
