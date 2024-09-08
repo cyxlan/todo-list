b33a76763dff78838b7822401624424142a6b818
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-09-08 17:30:28 -0400

    Display todo list on page

--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@ import { createProject, renameProject, deleteProject, getProjectNames, getProjec
 import { createTodo, toggleTodoComplete, editTodo, deleteTodo, changeTodoProject } from "./todo";
 
 const displayController = (function() {
-  const printTodoList = () => {
-    console.log("TO-DO LIST")
+  const contentDiv = document.querySelector("#content");
+
+  const updateDisplay = () => {
     for (const projectName of getProjectNames()) {
-      console.log("Project:", projectName);
+      const projectDiv = document.createElement("div");
+      const projectHeader = document.createElement("h2");
+
+      projectHeader.textContent = projectName;
+      projectDiv.append(projectHeader);
 
       const todos = getProjectTodos(projectName);
       if (todos.length === 0) {
-        console.log("No tasks yet")
+        const noTasksNote = document.createElement("p");
+        noTasksNote.textContent = "No tasks yet";
+        projectDiv.append(noTasksNote);
       } else {
         for (const todo of todos) {
-          console.log({...todo});
+          const todoArticle = document.createElement("article");
+
+          for (const property in todo) {
+            const p = document.createElement("p");
+            p.textContent = todo[property];
+            todoArticle.append(p);
+          }
+          projectDiv.append(todoArticle);
         }
       }
+      contentDiv.append(projectDiv);
     }
   }
 
-  printTodoList();
+  updateDisplay();
 })();
